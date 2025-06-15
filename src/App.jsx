ae14fca7fcc15150e6463470d83bd5a5b107c339
Ngoc-Nhan <nhanvtn7501@ut.edu.vn>
2025-06-15 19:58:47 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import Appbar from './components/Appbar/Appbar'
+
+
+function App() {
+
+
+  return (
+    <>
+      <Appbar/>
+     
+ 
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Appbar/Appbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from '@mui/material/Button'
+
+const Appbar = () => {
+  return (
+
+    <>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </>
+  )
+}
+
+
+export default Appbar
